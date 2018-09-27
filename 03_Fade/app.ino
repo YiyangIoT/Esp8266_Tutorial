bb8da0bea1677ce738b0f99cdae8de7e5ecfcbe4
Yiyang <chiangyiyang@gmail.com>
2018-09-28 00:10:26 +0800

    修改目錄名稱及腳位led=2

--- 03_Fade/app.ino
+++ 03_Fade/app.ino
@@ -1,17 +1,17 @@
 
-int led = 9;           // the PWM pin the LED is attached to
+int led = 2;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // declare pin 9 to be an output:
+  // declare pin 2 to be an output:
   pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // set the brightness of pin 9:
+  // set the brightness of pin 2:
   analogWrite(led, brightness);
 
   // change the brightness for next time through the loop:
